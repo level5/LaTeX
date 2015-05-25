cdb94f9d7faf8afea9daf87ff0ec4889df7f4891
Shifeng Huang <shifeng.huang@hp.com>
2015-05-25 19:38:17 +0800

    update

---
+++ programming/code/shell/abs-guide/chapter04/strings.sh
@@ -0,0 +1,66 @@
+# protecting a comand-line parameter from the shell
+
+# suppress echo's "appetite" for newlines
+
+echo ${ls -l}
+
+echo "${ls -l}"
+
+# "..." double quotes prevent reinterpretation of all special characters with the quoted string except $, `(backquote) and(escape).
+
+# '...' single quotes prevent reinterpretation of all special characters with the quoted string except '(singlequote).
+# so variable reference is turn off in single quotes.
+
+List="one two three"
+
+for a in $List
+do
+  echo "$a"
+done
+# one
+# two
+# three
+
+for a in "$List"
+do
+  echo "$a"
+done
+# one two three
+
+
+
+
+
+var="a variable containing five words"
+COMMAND This is $var
+# COMMAND with 7 arguments: This is a variable containing five words
+
+COMMAND "This is $var"
+# COMMAND with 1 argument: "This is a variable containing five words"
+
+
+var2=""
+
+COMMAND $var2 $var2 $var2
+# no argument
+
+COMMAND "$var2" "$var2" "$var2"
+# 3 empty arguments
+
+COMMAND "$var2 $var2 $var2"
+# 1 argument(2 spaces)
+
+
+
+# Echoing Weird Variables
+
+var="'(]\\{}\$\""
+
+echo $var         # '(]\{}$"
+echo "$var"       # '(]\{}$" same as above
+
+
+IFS='\'    # IFS
+
+echo $var         # '(] {}$"
+echo "$var"       # '(]\{}$"
--- programming/code/shell/abs-guide/useful-script/find-pattern-text.sh
+++ programming/code/shell/abs-guide/useful-script/find-pattern-text.sh
@@ -1,5 +1,7 @@
-# use GNU grep's feature
 
+# find pattern text
+
+# use GNU grep's feature
 grep -r --include "*.java" teststring /home/user1
 
 
@@ -10,5 +12,24 @@ grep -r --include "*.java" teststring /home/user1
 
 find /home/user1 -name '*.java' -exec grep teststring {} +
 
-
 find /home/user01 -name "*.java" -print0 | xargs -0 grep teststring
+
+
+
+
+# check if run by root user.
+ROOT_UID=0
+if ["$UID" -ne "$ROOT_UID"]; then
+  echo "not root user, please run with root."
+else
+  echo "running with root user."
+fi
+
+
+
+# add directory to PATH
+HOME=/java
+export PATH="$HOME/bin:$PATH"
+
+
+#
