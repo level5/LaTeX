bf592487c59c33a437f58785b7967537e9c18028
Shifeng Huang <shifeng.huang@hp.com>
2015-08-14 17:56:50 +0800

    update

--- programming/code/shell/abs-guide/chapter04/functions.sh
+++ programming/code/shell/abs-guide/chapter04/functions.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 # function function_name () {
 #   COMMAND
 # }
@@ -11,6 +11,7 @@
 
 
 # Function declaration must precede call
+# 函数定义必须出现在函数调用前面
 
 
 f1 ()
@@ -25,11 +26,35 @@ f2 ()
 }
 
 f1        # it works because f2 is not actually called until this point.
+          # 这个是可以的，因为f2实际的调用点是在这里。
 
 
 # function may not be empty.
 # a function containing only comments is empty.
-
+# 函数不能为空
+# 只含有注释的函数也是一个空函数
 
 
 # shell script only pass value parameters to function.
+
+
+
+# 函数可以嵌套，但是其实并没有什么卵用
+
+
+
+# 函数参数
+
+f3(){
+  if [[ -z "$1" ]]; then
+    echo "-Parameter #1 is zero length"
+  else
+    echo "-Parameter #1 is \"$1\""
+  fi
+}
+
+f3
+
+f3 ""
+
+f3 "kitta"
